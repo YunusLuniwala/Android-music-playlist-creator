45d715b783d66f82df912f812184f2196c868388
Sandeep Raghuraman <sandy.8925@gmail.com>
2012-03-30 03:15:09 -0400

    Added various members and getter and setter functions to the Playlist class

--- src/com/trinity/stooges/Playlist.java
+++ src/com/trinity/stooges/Playlist.java
@@ -1,5 +1,33 @@
 package com.trinity.stooges;
 
-class CleverPlaylist {
+class Playlist {
+	private int playlistId;
+	private String name;
+	private int numSongs;
+	private String songListTableName;
 	
+	public Playlist(String nm, int nmsngs, String snglsttblnm)
+	{
+		name = nm;
+		numSongs = nmsngs;
+		songListTableName = snglsttblnm;
+	}
+	
+	public String getName()
+	{ return name; }
+	
+	public int getNumSongs()
+	{ return numSongs; }
+	
+	public String getSongListTableName()
+	{ return songListTableName; }
+	
+	public void setName(String nm)
+	{ name = nm; }
+	
+	public void setNumSongs(int nmsngs)
+	{ numSongs = nmsngs; }
+	
+	public void setSongListTableName(String snglsttblnm)
+	{ songListTableName = snglsttblnm; }
 }
